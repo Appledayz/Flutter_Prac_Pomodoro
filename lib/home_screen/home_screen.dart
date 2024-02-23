25768376fdb2a2a3c2b54a4cb28a13b2f5d39b69
송동일 <song-di@asoosoft.net>
2024-02-24 02:06:48 +0900

    Stop

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -45,6 +45,14 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onStopPressed() {
+    _timer.cancel();
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      isRunning = false;
+    });
+  }
+
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
     return duration.toString().split('.').first.substring(2, 7);
@@ -71,17 +79,31 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Flexible(
               flex: 3,
-              child: Center(
-                  child: IconButton(
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(
-                  isRunning
-                      ? Icons.pause_circle_outline
-                      : Icons.play_circle_outline,
-                  size: 100,
-                  color: const Color(0xFF232B55),
-                ),
-              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: Icon(
+                      isRunning
+                          ? Icons.pause_circle_outline
+                          : Icons.play_circle_outline,
+                      size: 100,
+                      color: const Color(0xFF232B55),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  // reset button
+                  IconButton(
+                    onPressed: onStopPressed,
+                    icon: const Icon(
+                      Icons.stop_circle_outlined,
+                      size: 100,
+                      color: Color(0xFF232B55),
+                    ),
+                  )
+                ],
+              ),
             ),
             Flexible(
               flex: 1,
