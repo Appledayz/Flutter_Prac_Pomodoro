94bf59010d136a6767e5ad99e1d55e1c3122f8ef
송동일 <song-di@asoosoft.net>
2024-02-25 02:49:54 +0900

    update url

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "067499f114b2347cc20e6c3de5862512",
-"/": "067499f114b2347cc20e6c3de5862512",
+"index.html": "9648c9cb02d6cef9200b9ee2266be340",
+"/": "9648c9cb02d6cef9200b9ee2266be340",
 "main.dart.js": "3cefcd4171f41e83d3c0ac5e92b3447f",
 "manifest.json": "f6a519a083377c73d808df394da956c0",
 "version.json": "6466e544e28481662ac0bbbcbabbc638"};
