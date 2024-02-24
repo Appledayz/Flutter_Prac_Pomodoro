83e82cd713e946592f313c37c2c73a0a7428eee0
송동일 <song-di@asoosoft.net>
2024-02-25 03:07:50 +0900

    update docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,10 +27,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1a9e04dc589e856229a2c9c64b4fd0e1",
-"/": "1a9e04dc589e856229a2c9c64b4fd0e1",
+"index.html": "04e2a75e454bf0488945d4bc0866e098",
+"/": "04e2a75e454bf0488945d4bc0866e098",
 "main.dart.js": "3cefcd4171f41e83d3c0ac5e92b3447f",
-"manifest.json": "e2eef5b3d94abe98bf42e09421a7983b",
+"manifest.json": "f6a519a083377c73d808df394da956c0",
 "version.json": "6466e544e28481662ac0bbbcbabbc638"};
 // The application shell files that are downloaded before a service worker can
 // start.
